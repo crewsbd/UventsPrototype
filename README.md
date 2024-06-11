9468b9156abf4da5e569d620e00fc3880a22ff5a
crewsbd <bdcrews@protonmail.com>
2024-06-11 03:09:38 -0700

    Some basic setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# SETUP
+
+1. npm install
+2. create .env file
+3. populate fields: DEV, HOST, PORT, DB_URI
+4. run "Start Server" debug or enter npm start in the terminal
+
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const app = require('express')();
 
 
+console.log('routes');
 
-
-
+app.get('/', async (request, response) => {
+    response.status(202).json({ 
+        message: "ECHO QUERY STRINGS",
+        query: request.query });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`);
